e25b9e4b7975c6ccd712ea563b66595efbc70544
Jae seok Lee <dlwotjraks@gmail.com>
2025-05-22 21:55:42 -0700

    Update biggest_number.py

--- sort/biggest_number.py
+++ sort/biggest_number.py
@@ -1,4 +1,6 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x*10, reverse=True)
-    return ''.join(numbers)
+    answer =  ''.join(numbers)
+    return '0' if answer[0] == '0' else answer
+    
